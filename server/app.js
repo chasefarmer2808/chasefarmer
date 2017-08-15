05a500777a3c886dda48446af60880070193baca
Chase Farmer <chasefarmer2808@ufl.edu>
2017-08-15 16:57:48 -0400

    restarting

--- server/app.js
+++ server/app.js
@@ -1,43 +1,11 @@
-/**
- * Main application file
- */
-
 'use strict';
 
 import express from 'express';
-import mongoose from 'mongoose';
-mongoose.Promise = require('bluebird');
-import config from './config/environment';
-import http from 'http';
-import seedDatabaseIfNeeded from './config/seed';
 
-// Connect to MongoDB
-mongoose.connect(config.mongo.uri, config.mongo.options);
-mongoose.connection.on('error', function(err) {
-  console.error(`MongoDB connection error: ${err}`);
-  process.exit(-1); // eslint-disable-line no-process-exit
-});
+const app = express();
 
-// Setup server
-var app = express();
-var server = http.createServer(app);
-var socketio = require('socket.io')(server, {
-  serveClient: config.env !== 'production',
-  path: '/socket.io-client'
-});
-require('./config/socketio').default(socketio);
-require('./config/express').default(app);
-require('./routes').default(app);
+app.use(express.static('client'));
 
-// Start server
-function startServer() {
-  app.angularFullstack = server.listen(config.port, config.ip, function() {
-    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-  });
-}
-
-seedDatabaseIfNeeded();
-setImmediate(startServer);
-
-// Expose app
-exports = module.exports = app;
+app.listen(3000, function() {
+  console.log('listening on 3000');
+})
