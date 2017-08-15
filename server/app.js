d2e35962d3086b83cba83b548c872431dd1fc834
Chase Farmer <chasefarmer2808@ufl.edu>
2017-08-15 17:02:35 -0400

    server is running

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 'use strict';
 
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.static('client'));
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+  console.log('someone is on the website');
+});
 
 app.listen(3000, function() {
   console.log('listening on 3000');
-})
+});
