349fc3a108f671b232f6e8271f70435380c88b7a
Chase Farmer <chasefarmer2808@ufl.edu>
2017-08-20 13:46:31 -0400

    about content

--- client/components/about/about.controller.js
+++ client/components/about/about.controller.js
@@ -8,6 +8,16 @@
   function AboutController() {
     var vm = this;
 
-    vm.test = 'about view';
+    vm.message = `My name is Chase Farmer.
+                  I am a 9th semester Computer Engineering student at the University of Florida.  I have a
+                  passion for full stack web development.  I made this website
+                  from scratch using NodeJS and AngularJS.  I have used many
+                  web frameworks to create client facing applications.  I also
+                  have a passion for embedded, real time applications and IoT.
+                  I have used several microprocessors and microcontrollers and
+                  interfaced them with other electronics through various
+                  communication protocals.  If your company needs someone with
+                  my skill set, please reach out to me.
+                  `;
   };
 })();
